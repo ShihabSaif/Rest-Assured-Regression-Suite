1e47556d333ff8147246eebdcd1b18af571f0753
A.B.M.Shihab Uddin <a.b.m.shihab57@gmail.com>
2025-04-09 15:58:24 +0600

    partial test case added for card add money

---
+++ src/com/example/restassuredproject/DTO/AddMoneyCardBodyParams.java
@@ -0,0 +1,2 @@
+package com.example.restassuredproject.DTO;public class AddMoneyCardBodyParams {
+}
---
+++ src/com/example/restassuredproject/features/AddMoneyFromCard.java
@@ -0,0 +1,2 @@
+package com.example.restassuredproject.features;public class AddMoneyFromCard {
+}
